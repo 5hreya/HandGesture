dbd6386613d8defedc7ec6751af19faacd1c4f78
Shreya <73158356+5hreya@users.noreply.github.com>
2023-04-11 17:45:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 //https://teachablemachine.withgoogle.com/models/J2tuBbTBR/
+
+prediction = ""
+
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: "png",
+    png_quality: 90,
+})
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera)
+
+function capture_img(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "img" src = "'+data_uri+'"/>'
+    })
+}
+
+console.log("ml5 version", ml5.version)
+
+addmodel = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/4HPZO0Ku-/model.json", model_loaded)
+
+function model_loaded(){
+    console.log("Model has loaded.")
+}
